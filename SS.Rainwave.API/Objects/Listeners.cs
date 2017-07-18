33e6539c27c6e337a04ffbf6ae10eb63a1cfa843
James McCollum <jmccollum@roydan.com>
2017-07-18 14:17:30 -0500

    Update Listeners object

--- SS.Rainwave.API/Objects/Listeners.cs
+++ SS.Rainwave.API/Objects/Listeners.cs
@@ -1,15 +1,33 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using SS.Rainwave.API.Objects.Support;
 
 namespace SS.Rainwave.API.Objects
 {
 	[JsonObject]
-	public class Listeners
+	public class Listener
 	{
-		[JsonProperty(PropertyName = "guests")]
-		public int Guests { get; set; }
-
-		[JsonProperty(PropertyName = "users")]
-		public List<User> Users { get; set; }
+		public string Rank { get; set; }
+		public int WinningVotes { get; set; }
+		public int TotalVotes { get; set; }
+		public int TotalRatings { get; set; }
+		public int UserId { get; set; }
+		public List<RequestCount> RequestsBySourceStation { get; set; }
+		public List<RatingCount> RatingSpread { get; set; }
+		public int LosingRequests { get; set; }
+		public string Avatar { get; set; }
+		public int MindChanges { get; set; }
+		public int WinningRequests { get; set; }
+		public List<StationRating> RatingsByStation { get; set; }
+		public int LosingVotes { get; set; }
+		public string Name { get; set; }
+		public List<RequestCount> RequestsByStation { get; set; }
+		public List<StationVotes> VotesByStation { get; set; }
+		public string Colour { get; set; }
+		public int RegDate { get; set; }
+		public List<Album> TopAlbums { get; set; }
+		public RatingCompletion RatingCompletion { get; set; }
+		public List<Album> TopRequestAlbums { get; set; }
+		public int TotalRequests { get; set; }
 	}
 }
---
+++ SS.Rainwave.API/Objects/Support/RatingCompletion.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SS.Rainwave.API.Objects.Support
+{
+	[JsonObject]
+	public class RatingCompletion
+	{
+		[JsonProperty(PropertyName = "1")]
+		public int Game { get; set; }
+		[JsonProperty(PropertyName = "2")]
+		public int OcRemix { get; set; }
+		[JsonProperty(PropertyName = "3")]
+		public int Covers { get; set; }
+		[JsonProperty(PropertyName = "4")]
+		public int Chiptunes { get; set; }
+	}
+}
